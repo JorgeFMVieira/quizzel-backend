4c99d71c7bcb288aded7a075f8335ab6da63fb9d
welel <jocafmv@gmail.com>
2022-08-15 13:54:27 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (req: any) => {
+app.get("/", (req) => {
     console.log("Hello World");
 });
 
 app.listen(3001, () => {
-    console.log("server listening on port");
+    console.log("server listening on port 3001");
 });
 
